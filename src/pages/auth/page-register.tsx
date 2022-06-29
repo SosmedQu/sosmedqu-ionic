034eb42e88e170a468abcefcae42dc9c501c3a6a
CROWNIX <rahmatfauzi841@gmail.com>
2022-06-29 15:53:47 +0700

    Integrasi API

--- src/pages/auth/page-register.tsx
+++ src/pages/auth/page-register.tsx
@@ -1,76 +1,79 @@
-import { chevronBackCircleOutline, chevronForwardCircleOutline } from 'ionicons/icons';
-import { IonAlert, IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonImg, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
-import './auth.css';
-import Axios from 'axios';
-import { useState } from 'react';
-import MyAlert from '../../components/Alert';
-import MyApi from '../../helpers/my-api';
-
-
-
+import {chevronBackCircleOutline, chevronForwardCircleOutline} from "ionicons/icons";
+import {IonAlert, IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonImg, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar} from "@ionic/react";
+import "./auth.css";
+import Axios from "axios";
+import {useState} from "react";
+import MyAlert from "../../components/Alert";
+import MyApi from "../../helpers/my-api";
+import {useHistory} from "react-router";
 
 const Register: React.FC = () => {
+    const history = useHistory();
     interface IAlert {
-        type: string
-        show: boolean
-        header: string
-        msg: string
-        buttons: any
+        type: string;
+        show: boolean;
+        header: string;
+        msg: string;
+        buttons: any;
     }
-    const url = "http://localhost:3000/api/auth/register";
     const [email, setEmail] = useState("");
-    const [Alert, setAlert] = useState<IAlert>(
-        {
-            type: "",
-            show: false,
-            msg: "",
-            header: "",
-            buttons: undefined
-        }
-    );
+    const [Alert, setAlert] = useState<IAlert>({
+        type: "",
+        show: false,
+        msg: "",
+        header: "",
+        buttons: undefined,
+    });
 
     const onSubmit = (e: any) => {
         e.preventDefault();
         const api = new MyApi();
-        api.register({ email: email, link: "http://localhost:8100/register/create-password" }).then((response) => {
-            console.log(response);
-            setAlert({
-                type: "success",
-                show: true,
-                header: "Berhasil",
-                msg: response.data.msg,
-                buttons: [
-                    {
-                        text: 'OK',
-                        handler: () => {
-                            console.log("Test Success")
-                        }
-                    }
-                ]
-            })
-            console.log(Alert);
-        }, (err) => {
-            setAlert({
-                type: "failed",
-                header: "Gagal",
-                show: true,
-                msg: err,
-                buttons: [
-                    {
-                        text: 'OK',
-                        handler: () => {
-                            console.log("Test Success")
-                        }
-                    }
-                ]
-            })
-        });
-    }
+        api.register({email: email, link: "http://localhost:8100/register/create-password"}).then(
+            (response) => {
+                console.log(response);
+                setAlert({
+                    type: "success",
+                    show: true,
+                    header: "Berhasil",
+                    msg: response.data.msg,
+                    buttons: [
+                        {
+                            text: "OK",
+                            handler: () => {
+                                history.push({
+                                    pathname: "/register/verify-email",
+                                    state: email,
+                                });
+                            },
+                        },
+                    ],
+                });
+                console.log(Alert);
+            },
+            (err) => {
+                console.log(err);
+                setAlert({
+                    type: "failed",
+                    header: "Gagal",
+                    show: true,
+                    msg: err.response.data.errors[0].msg,
+                    buttons: [
+                        {
+                            text: "OK",
+                            handler: () => {
+                                console.log("Test Success");
+                            },
+                        },
+                    ],
+                });
+            }
+        );
+    };
 
     const onTyping = (e: any) => {
         console.log(e.detail.value);
         setEmail(e.detail.value);
-    }
+    };
     return (
         <IonPage>
             <IonHeader>
@@ -85,14 +88,8 @@ const Register: React.FC = () => {
                     </IonToolbar>
                 </IonHeader>
                 <div className="container">
-                    <IonAlert 
-                    isOpen={Alert.show}
-                    header={Alert.header}
-                    cssClass={'text-center alert-' + Alert.type}
-                    message={Alert.msg}
-                    buttons={Alert.buttons}
-                    />
-                    <form onSubmit={e => onSubmit(e)}>
+                    <IonAlert isOpen={Alert.show} header={Alert.header} cssClass={"text-center alert-" + Alert.type} message={Alert.msg} buttons={Alert.buttons} />
+                    <form onSubmit={(e) => onSubmit(e)}>
                         <IonGrid>
                             <IonRow>
                                 <IonCol className="flex-center">
@@ -103,12 +100,7 @@ const Register: React.FC = () => {
                                 <IonCol>
                                     <IonItem>
                                         <IonLabel position="floating">Email Address</IonLabel>
-                                        <IonInput
-                                            type="email"
-                                            value={email}
-                                            name="email"
-                                            onIonChange={e => onTyping(e!)}
-                                        ></IonInput>
+                                        <IonInput type="email" value={email} name="email" onIonChange={(e) => onTyping(e!)}></IonInput>
                                     </IonItem>
                                 </IonCol>
                             </IonRow>
@@ -117,7 +109,7 @@ const Register: React.FC = () => {
                                     <IonIcon icon={chevronBackCircleOutline} slot="start"></IonIcon>
                                     <IonLabel>Back</IonLabel>
                                 </IonButton>
-                                <IonButton type='submit' color="light">
+                                <IonButton type="submit" color="light">
                                     <IonIcon icon={chevronForwardCircleOutline} slot="end"></IonIcon>
                                     <IonLabel>Next</IonLabel>
                                 </IonButton>
@@ -127,7 +119,7 @@ const Register: React.FC = () => {
                 </div>
             </IonContent>
         </IonPage>
-    )
-}
+    );
+};
 
 export default Register;
--- src/pages/auth/page-verify-email.tsx
+++ src/pages/auth/page-verify-email.tsx
@@ -1,7 +1,69 @@
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonGrid, IonRow, IonCol, IonButton, IonLabel } from "@ionic/react"
-import { mailOutline, sendSharp, mailSharp } from "ionicons/icons"
+import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonGrid, IonRow, IonCol, IonButton, IonLabel, IonAlert} from "@ionic/react";
+import {mailOutline, sendSharp, mailSharp} from "ionicons/icons";
+import {useState} from "react";
+import {useLocation} from "react-router";
+import MyApi from "../../helpers/my-api";
 
 const VerifikasiEmail: React.FC = () => {
+    interface IAlert {
+        type: string;
+        show: boolean;
+        header: string;
+        msg: string;
+        buttons: any;
+    }
+    const [Alert, setAlert] = useState<IAlert>({
+        type: "",
+        show: false,
+        msg: "",
+        header: "",
+        buttons: undefined,
+    });
+
+    const location = useLocation();
+    const resendEmail = () => {
+        const email = location.state ? location.state : null;
+
+        const api = new MyApi();
+        api.register({email: email, link: "http://localhost:8100/register/create-password"}).then(
+            (response) => {
+                console.log(response);
+                setAlert({
+                    type: "success",
+                    show: true,
+                    header: "Berhasil",
+                    msg: response.data.msg,
+                    buttons: [
+                        {
+                            text: "OK",
+                        },
+                    ],
+                });
+            },
+            (err) => {
+                setAlert({
+                    type: "failed",
+                    header: "Gagal",
+                    show: true,
+                    msg: err.response.data.errors[0].msg,
+                    buttons: [
+                        {
+                            text: "OK",
+                        },
+                    ],
+                });
+            }
+        );
+    };
+    const resetAlert = () => {
+        setAlert({
+            type: "",
+            show: false,
+            msg: "",
+            header: "",
+            buttons: undefined,
+        });
+    };
 
     return (
         <IonPage>
@@ -17,14 +79,20 @@ const VerifikasiEmail: React.FC = () => {
                     </IonToolbar>
                 </IonHeader>
                 <div className="container">
-                    <IonIcon icon={mailOutline} style={{ "font-size": "150px" }}></IonIcon>
+                    <IonAlert isOpen={Alert.show} header={Alert.header} cssClass={"text-center alert-" + Alert.type} message={Alert.msg} buttons={Alert.buttons} onDidDismiss={() => resetAlert()} />
+                    <IonIcon icon={mailOutline} style={{"font-size": "150px"}}></IonIcon>
                     <h1>Verify Your Email</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit ame venenatis, <a>Click Resend Verify</a></p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla
+                        est
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit ame venenatis, <a>Click Resend Verify</a>
+                    </p>
                     <IonGrid>
                         <IonRow>
                             <IonCol size="12">
-                                <IonButton color="light">
+                                <IonButton color="light" onClick={() => resendEmail()}>
                                     <IonIcon icon={sendSharp} slot="start"></IonIcon>
                                     <IonLabel>Resend Verify</IonLabel>
                                 </IonButton>
@@ -40,7 +108,7 @@ const VerifikasiEmail: React.FC = () => {
                 </div>
             </IonContent>
         </IonPage>
-    )
-}
+    );
+};
 
 export default VerifikasiEmail;
